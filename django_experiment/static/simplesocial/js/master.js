e8c9007b7b9ed3e766e800acb9f64963b2f2633b
Prakash Karuppannan Govindaraj <prakashkarupannan.govindaraj@impigertech.com>
2018-06-22 19:36:01 +0530

    Added login and signup designs

--- django_experiment/static/simplesocial/js/master.js
+++ django_experiment/static/simplesocial/js/master.js
@@ -203,3 +203,30 @@ function giveBirth(e, u) {
 
   if (u > 1) giveBirth(e, u - 1);
 }
+
+// $(function() {
+//     $('#login-form-link').click(function(e) {
+//     	$("#login-form").delay(100).fadeIn(100);
+//  		$("#register-form").fadeOut(100);
+// 		$('#register-form-link').removeClass('active');
+// 		$(this).addClass('active');
+// 		e.preventDefault();
+// 	});
+// 	$('#register-form-link').click(function(e) {
+// 		$("#register-form").delay(100).fadeIn(100);
+//  		$("#login-form").fadeOut(100);
+// 		$('#login-form-link').removeClass('active');
+// 		$(this).addClass('active');
+// 		e.preventDefault();
+// 	});
+//
+// });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
